968a5311b53c10d0205bb7acfc9daba5ec9ca976
Gerardo Castillo <gerrcass@gmail.com>
2021-01-03 11:43:39 -0400

    testing code

---
+++ src/data/products.js
@@ -0,0 +1,304 @@
+const data = [
+  {
+    id: 1,
+    name: "Brocolli - 1 Kg",
+    nombre: "Brocolis - 1 Kg",
+    price: 120,
+    image:
+      "https://res.cloudinary.com/sivadass/image/upload/v1493620046/dummy-products/broccoli.jpg",
+    category: "vegetables",
+    categoria: "vegetales",
+  },
+  {
+    id: 2,
+    name: "Cauliflower - 1 Kg",
+    nombre: "Coliflor - 1 Kg",
+    price: 60,
+    image:
+      "https://res.cloudinary.com/sivadass/image/upload/v1493620046/dummy-products/cauliflower.jpg",
+    category: "vegetables",
+    categoria: "vegetales",
+  },
+  {
+    id: 3,
+    name: "Cucumber - 1 Kg",
+    nombre: "Pepinos - 1 Kg",
+    price: 48,
+    image:
+      "https://res.cloudinary.com/sivadass/image/upload/v1493620046/dummy-products/cucumber.jpg",
+    category: "vegetables",
+    categoria: "vegetales",
+  },
+  {
+    id: 4,
+    name: "Beetroot - 1 Kg",
+    nombre: "Remolachas - 1 Kg",
+    price: 32,
+    image:
+      "https://res.cloudinary.com/sivadass/image/upload/v1493620045/dummy-products/beetroot.jpg",
+    category: "vegetables",
+    categoria: "vegetales",
+  },
+  {
+    id: 5,
+    name: "Carrot - 1 Kg",
+    nombre: "Zanahorias - 1 Kg",
+    price: 56,
+    image:
+      "https://res.cloudinary.com/sivadass/image/upload/v1493620046/dummy-products/carrots.jpg",
+    category: "vegetables",
+    categoria: "vegetales",
+  },
+  {
+    id: 6,
+    name: "Tomato - 1 Kg",
+    nombre: "Tomates - 1 Kg",
+    price: 16,
+    image:
+      "https://res.cloudinary.com/sivadass/image/upload/v1493620045/dummy-products/tomato.jpg",
+    category: "vegetables",
+    categoria: "vegetales",
+  },
+  {
+    id: 7,
+    name: "Beans - 1 Kg",
+    nombre: "Vainitas - 1 Kg",
+    price: 82,
+    image:
+      "https://res.cloudinary.com/sivadass/image/upload/v1493620045/dummy-products/beans.jpg",
+    category: "vegetables",
+    categoria: "vegetales",
+  },
+  {
+    id: 8,
+    name: "Brinjal - 1 Kg",
+    nombre: "Berenjenas - 1 Kg",
+    price: 35,
+    image:
+      "https://res.cloudinary.com/sivadass/image/upload/v1493620045/dummy-products/brinjal.jpg",
+    category: "vegetables",
+    categoria: "vegetales",
+  },
+  {
+    id: 9,
+    name: "Capsicum",
+    nombre: "Pimentones",
+    price: 60,
+    image:
+      "https://res.cloudinary.com/sivadass/image/upload/v1493620045/dummy-products/capsicum.jpg",
+    category: "vegetables",
+    categoria: "vegetales",
+  },
+  {
+    id: 10,
+    name: "Mushroom - 1 Kg",
+    nombre: "Hongos - 1 Kg",
+    price: 75,
+    image:
+      "https://res.cloudinary.com/sivadass/image/upload/v1493620045/dummy-products/button-mushroom.jpg",
+    category: "vegetables",
+    categoria: "vegetales",
+  },
+  {
+    id: 11,
+    name: "Potato - 1 Kg",
+    nombre: "Papas - 1 Kg",
+    price: 22,
+    image:
+      "https://res.cloudinary.com/sivadass/image/upload/v1493620045/dummy-products/potato.jpg",
+    category: "vegetables",
+    categoria: "vegetales",
+  },
+  {
+    id: 12,
+    name: "Pumpkin - 1 Kg",
+    nombre: "Calabazas - 1 Kg",
+    price: 48,
+    image:
+      "https://res.cloudinary.com/sivadass/image/upload/v1493620045/dummy-products/pumpkin.jpg",
+    category: "vegetables",
+    categoria: "vegetales",
+  },
+  {
+    id: 13,
+    name: "Corn - 1 Kg",
+    nombre: "Maíz - 1 Kg",
+    price: 75,
+    image:
+      "https://res.cloudinary.com/sivadass/image/upload/v1493620045/dummy-products/corn.jpg",
+    category: "vegetables",
+    categoria: "vegetales",
+  },
+  {
+    id: 14,
+    name: "Onion - 1 Kg",
+    nombre: "Cebollas - 1 Kg",
+    price: 16,
+    image:
+      "https://res.cloudinary.com/sivadass/image/upload/v1493620045/dummy-products/onion.jpg",
+    category: "vegetables",
+    categoria: "vegetales",
+  },
+  {
+    id: 15,
+    name: "Apple - 1 Kg",
+    nombre: "Manzanas - 1 Kg",
+    price: 72,
+    image:
+      "https://res.cloudinary.com/sivadass/image/upload/v1493620045/dummy-products/apple.jpg",
+    category: "fruits",
+    categoria: "frutas",
+  },
+  {
+    id: 16,
+    name: "Banana - 1 Kg",
+    nombre: "Bananas - 1 Kg",
+    price: 45,
+    image:
+      "https://res.cloudinary.com/sivadass/image/upload/v1493620045/dummy-products/banana.jpg",
+    category: "fruits",
+    categoria: "frutas",
+  },
+  {
+    id: 17,
+    name: "Grapes - 1 Kg",
+    nombre: "Uvas - 1 Kg",
+    price: 60,
+    image:
+      "https://res.cloudinary.com/sivadass/image/upload/v1493620045/dummy-products/grapes.jpg",
+    category: "fruits",
+    categoria: "frutas",
+  },
+  {
+    id: 18,
+    name: "Mango - 1 Kg",
+    nombre: "Mangos - 1 Kg",
+    price: 75,
+    image:
+      "https://res.cloudinary.com/sivadass/image/upload/v1493620045/dummy-products/mango.jpg",
+    category: "fruits",
+    categoria: "frutas",
+  },
+  {
+    id: 19,
+    name: "Musk Melon - 1 Kg",
+    nombre: "Melón - 1 Kg",
+    price: 36,
+    image:
+      "https://res.cloudinary.com/sivadass/image/upload/v1493620045/dummy-products/musk-melon.jpg",
+    category: "fruits",
+    categoria: "frutas",
+  },
+  {
+    id: 20,
+    name: "Orange - 1 Kg",
+    nombre: "Naranjas - 1 Kg",
+    price: 75,
+    image:
+      "https://res.cloudinary.com/sivadass/image/upload/v1493620045/dummy-products/orange.jpg",
+    category: "fruits",
+    categoria: "frutas",
+  },
+  {
+    id: 21,
+    name: "Pears - 1 Kg",
+    nombre: "Peras - 1 Kg",
+    price: 69,
+    image:
+      "https://res.cloudinary.com/sivadass/image/upload/v1493620045/dummy-products/pears.jpg",
+    category: "fruits",
+    categoria: "frutas",
+  },
+  {
+    id: 22,
+    name: "Pomegranate - 1 Kg",
+    nombre: "Granada - 1 Kg",
+    price: 95,
+    image:
+      "https://res.cloudinary.com/sivadass/image/upload/v1493620045/dummy-products/pomegranate.jpg",
+    category: "fruits",
+    categoria: "frutas",
+  },
+  {
+    id: 23,
+    name: "Raspberry - 1/4 Kg",
+    nombre: "Frambuesa - 1/4 Kg",
+    price: 160,
+    image:
+      "https://res.cloudinary.com/sivadass/image/upload/v1493620045/dummy-products/raspberry.jpg",
+    category: "fruits",
+    categoria: "frutas",
+  },
+  {
+    id: 24,
+    name: "Strawberry - 1/4 Kg",
+    nombre: "Fresas - 1/4 Kg",
+    price: 180,
+    image:
+      "https://res.cloudinary.com/sivadass/image/upload/v1493620045/dummy-products/strawberry.jpg",
+    category: "fruits",
+    categoria: "frutas",
+  },
+  {
+    id: 25,
+    name: "Water Melon - 1 Kg",
+    nombre: "Patilla - 1 Kg",
+    price: 28,
+    image:
+      "https://res.cloudinary.com/sivadass/image/upload/v1493620045/dummy-products/water-melon.jpg",
+    category: "fruits",
+    categoria: "frutas",
+  },
+  {
+    id: 26,
+    name: "Almonds - 1/4 Kg",
+    nombre: "Almendras - 1/4 Kg",
+    price: 200,
+    image:
+      "https://res.cloudinary.com/sivadass/image/upload/v1493620045/dummy-products/almonds.jpg",
+    category: "nuts",
+    categoria: "nueces",
+  },
+  {
+    id: 27,
+    name: "Pista - 1/4 Kg",
+    nombre: "Pistacho - 1/4 Kg",
+    price: 190,
+    image:
+      "https://res.cloudinary.com/sivadass/image/upload/v1493620045/dummy-products/pista.jpg",
+    category: "nuts",
+    categoria: "nueces",
+  },
+  {
+    id: 28,
+    name: "Nuts Mixture - 1 Kg",
+    nombre: "Frustos Secos - 1 Kg",
+    price: 950,
+    image:
+      "https://res.cloudinary.com/sivadass/image/upload/v1493620045/dummy-products/nuts-mixture.jpg",
+    category: "nuts",
+    categoria: "nueces",
+  },
+  {
+    id: 29,
+    name: "Cashews - 1 Kg",
+    nombre: "Merey - 1 Kg",
+    price: 650,
+    image:
+      "https://res.cloudinary.com/sivadass/image/upload/v1493620045/dummy-products/cashews.jpg",
+    category: "nuts",
+    categoria: "nueces",
+  },
+  {
+    id: 30,
+    name: "Walnuts - 1/4 Kg",
+    nombre: "Nueces - 1/4 Kg",
+    price: 170,
+    image:
+      "https://res.cloudinary.com/sivadass/image/upload/v1493620045/dummy-products/walnuts.jpg",
+    category: "nuts",
+    categoria: "nueces",
+  },
+];
+
+export default data;
